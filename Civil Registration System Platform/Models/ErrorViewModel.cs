123c44efef92346749d0f1185276f716a4b4c9cd
Mena Magdy <menamagdy475@gmail.com>
2026-04-29 17:54:49 +0300

    Initial Push For CRS

---
+++ Civil Registration System Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Civil_Registration_System_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
