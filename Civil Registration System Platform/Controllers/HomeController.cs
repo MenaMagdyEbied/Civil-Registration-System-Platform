ec91e56fc23266dcaa1767a1b88257ef0eaf2c83
Doaa Ashraf <doaaelshafei1@gmail.com>
2026-05-03 23:07:50 +0300

    Home Controller

--- Civil Registration System Platform/Controllers/HomeController.cs
+++ Civil Registration System Platform/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 
+using Civil_Registration_System_Platform.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,8 +7,19 @@ namespace Civil_Registration_System_Platform.Controllers
 {
     public class HomeController : Controller
     {
+<<<<<<< Updated upstream
         private readonly IAccountServices accountServices;
         public HomeController(IAccountServices accountServices)
+=======
+        private readonly IApplicationService _applicationService;
+
+        public HomeController(IApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+        [HttpGet]
+        public IActionResult Index()
+>>>>>>> Stashed changes
         {
             this.accountServices = accountServices;
         }
@@ -15,11 +27,33 @@ namespace Civil_Registration_System_Platform.Controllers
         {     
             return View();
         }
-
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Track()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Track(string applicationNumber)
+        {
+            if (string.IsNullOrWhiteSpace(applicationNumber))
+            {
+                ModelState.AddModelError(string.Empty, "من فضلك أدخل رقم الطلب.");
+                return View();
+            }
+
+            var application = await _applicationService.TrackApplicationAsync(applicationNumber);
+
+            if (application == null)
+            {
+                TempData["Error"] = "لم يتم العثور على طلب بهذا الرقم.";
+                return View();
+            }
+
+            return View("TrackResult", application);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
